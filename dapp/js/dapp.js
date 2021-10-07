212368c5340e4098ace3c579210b82dd05f57bf0
markcarey <mark@markcarey.com>
2021-10-07 00:55:13 -0400

    more current auction

--- dapp/js/dapp.js
+++ dapp/js/dapp.js
@@ -81,6 +81,8 @@ async function currentAuction() {
     $("#dog-title").text("Dog " + a.dogId);
     var date = moment.utc(a.startTime, "X").format("MMMM D YYYY");
     $("#dog-date").text(date);
+    var currentBid = web3.utils.fromWei(a.amount).toFixed(2);
+    $("#dog-min-bid").text(currentBid);
     var minBid = web3.utils.fromWei(a.amount) * 1.1
     $("#dog-min-bid").text(minBid.toFixed(2));
 }

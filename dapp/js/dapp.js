27bbf0abe3cab9ed81479eb5c801d2d9c2add0f3
markcarey <mark@markcarey.com>
2021-10-07 19:11:41 -0400

    fromWei

--- dapp/js/dapp.js
+++ dapp/js/dapp.js
@@ -79,13 +79,13 @@ async function connectWallet() {
 
 async function getFlows() {
     var dogBalance = await cDAIx.methods.balanceOf(dogAddress).call();
-    console.log(dogBalance);
+    console.log(web3.utils.fromWei(dogBalance));
     var userBalance = await cDAIx.methods.balanceOf(ethereum.selectedAddress).call();
-    console.log(userBalance);
+    console.log(web3.utils.fromWei(userBalance));
     var dogFlow = await cfa.methods.getNetFlow("0x3ED99f859D586e043304ba80d8fAe201D4876D57", dogAddress).call();
-    console.log(dogFlow);
+    console.log(web3.utils.fromWei(dogFlow));
     var userFlow = await cfa.methods.getFlow("0x3ED99f859D586e043304ba80d8fAe201D4876D57", dogAddress, ethereum.selectedAddress).call();
-    console.log(userFlow);
+    console.log(web3.utils.fromWei(userFlow));
 }
 
 var timer;

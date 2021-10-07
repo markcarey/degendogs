3468fa07a0c4fdc1771764d5ff2751e23c646ddc
markcarey <mark@markcarey.com>
2021-10-07 11:07:58 -0400

    bids html

--- dapp/js/dapp.js
+++ dapp/js/dapp.js
@@ -75,10 +75,10 @@ async function connectWallet() {
 } // connectWallet()
 
 async function auctionEvents(dogId) {
-    //const covEventsUrl = "https://api.covalenthq.com/v1/42/events/topics/0x1159164c56f277e6fc99c11731bd380e0347deb969b75523398734c252706ea3/?starting-block=27539184&ending-block=latest&sender-address=" + auctionAddress + "&key=ckey_ac7c55f53e19476b85f0a1099af";
-    //const response = await fetch(covEventsUrl);
-    //var covEvents = await response.json();
-    //console.log(covEvents);
+    const covEventsUrl = "https://api.covalenthq.com/v1/42/events/topics/0x1159164c56f277e6fc99c11731bd380e0347deb969b75523398734c252706ea3/?starting-block=27539184&ending-block=latest&sender-address=" + auctionAddress + "&key=ckey_ac7c55f53e19476b85f0a1099af";
+    const response = await fetch(covEventsUrl);
+    var covEvents = await response.json();
+    console.log(covEvents);
     var dogIdTopic = web3.utils.padLeft(web3.utils.toHex(dogId), 64);
     console.log(dogIdTopic);
     //console.log(web3.utils.toHex(dogId));
@@ -87,18 +87,23 @@ async function auctionEvents(dogId) {
         topics: ["0x1159164c56f277e6fc99c11731bd380e0347deb969b75523398734c252706ea3", dogIdTopic],
         fromBlock: 27539184
     });
+    logs = covEvents.data.items;
+    var bidsHTML = "";
     $.each(logs, function(index, log) {
         console.log(log);
-        var event = web3.eth.abi.decodeParameters(['address', 'uint256', 'bool'], log.data);
+        var event = web3.eth.abi.decodeParameters(['address', 'uint256', 'bool'], log.raw_log_data);
         console.log(event);
         var amt = parseFloat(web3.utils.fromWei( event[1] ));
         var bid = {
             "bidder": event[0],
             "bid": amt.toFixed(2),
-            "txn": log.transactionHash
+            "txn": log.tx_hash,
+            "date": log.block_signed_at
         };
         console.log(bid);
+        bidsHTML += getBidRowHTML(bid)
     });
+    console.log(bidsHTML);
 }
 
 async function currentAuction() {
@@ -115,5 +120,44 @@ async function currentAuction() {
     auctionEvents(a.dogId);
 }
 currentAuction();
+
+
+
+
+
+
+
+
+// HTML templates
+function getBidRowHTML(bid) {
+    var address = abbrAddress(bid.bidder);
+    var date = moment(bid.date).format("MMMM DD at HH:mm");
+    var html = `
+        <li class="BidHistory_bidRow__bc1Zf">
+            <div class="BidHistory_bidItem__13g5O">
+            <div class="BidHistory_leftSectionWrapper__2T29z">
+                <div class="BidHistory_bidder__1hPgQ">
+                <div>${address}</div>
+                </div>
+                <div class="BidHistory_bidDate__32l9k">${date}</div>
+            </div>
+            <div class="BidHistory_rightSectionWrapper__3oem5">
+                <div class="BidHistory_bidAmount__7W-Hz">Ξ ${bid.bid}</div>
+                <div class="BidHistory_linkSymbol__29ypX"><a
+                    href="https://kovan.etherscan.io/tx/${bid.txn}"
+                    target="_blank" rel="noreferrer"><svg aria-hidden="true" focusable="false"
+                    data-prefix="fas" data-icon="external-link-alt"
+                    class="svg-inline--fa fa-external-link-alt fa-w-16 " role="img"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor"
+                        d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z">
+                    </path>
+                    </svg></a></div>
+            </div>
+            </div>
+        </li>
+    `;
+    return html;
+}
 
 

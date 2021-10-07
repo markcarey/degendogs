bfda5608f44fa51b3834821414f71885a9699811
markcarey <mark@markcarey.com>
2021-10-07 01:05:27 -0400

    more bugs

--- dapp/js/dapp.js
+++ dapp/js/dapp.js
@@ -81,8 +81,8 @@ async function currentAuction() {
     $("#dog-title").text("Dog " + a.dogId);
     var date = moment.utc(a.startTime, "X").format("MMMM D YYYY");
     $("#dog-date").text(date);
-    var currentBid = web3.utils.fromWei(a.amount);
-    $(".dog-current-bid").text(currentBid.toFixed(2));
+    var currentBid = parseFloat(web3.utils.fromWei(a.amount));
+    $(".dog-current-bid").text("Ξ " + currentBid.toFixed(2));
     var minBid = web3.utils.fromWei(a.amount) * 1.1
     $("#dog-min-bid").text(minBid.toFixed(2));
 }

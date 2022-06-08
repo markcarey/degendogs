ad3f3737521a89f25b36db22c74a3214e2d55958
markcarey <mark@markcarey.com>
2022-06-08 14:21:48 -0400

    bid history

--- dapp/js/dapp.js
+++ dapp/js/dapp.js
@@ -457,7 +457,7 @@ async function currentAuction(thisDog) {
     } 
     var startingBlock = 26060775;
     startingBlock = 25818530; // auction creation block
-    startingBlock = 27511830; // fix this....
+    startingBlock = 28011830; // fix this....
     //console.log(a);
     const imageUrl = "https://api.degendogs.club/images/" + a.dogId + ".png";
     var tempImage = new Image();
